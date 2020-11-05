9fcfcdba15c9ca749d7f74fd0cae946b782ca372
Arturo Barajas saavedra <arturo.barajas@wolterskluwer.com>
2020-11-05 15:19:10 -0600

    End of section 11

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -45,4 +45,13 @@ export class MembersService {
       })
     );
   }
+
+  // tslint:disable-next-line: typedef
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+  }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId, {});
+  }
 }
